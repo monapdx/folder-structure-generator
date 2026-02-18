cf756dde82d69db24f57523de8f9bde6eda70d2b
Ashly <thegirlnextfloor@gmail.com>
2026-02-17 22:43:13 -0800

    N/A

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// GitHub Pages project site URL:
-// https://monapdx.github.io/folder-structure-generator/
 export default defineConfig({
+  // GitHub Pages project site:
+  // https://monapdx.github.io/folder-structure-generator/
   base: '/folder-structure-generator/',
   plugins: [react()],
 })
