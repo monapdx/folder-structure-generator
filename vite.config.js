35591fbab31410110633e5e77750b1b067d51142
Ashly <thegirlnextfloor@gmail.com>
2026-02-17 22:33:14 -0800

    N/A

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/folder-structure-generator/',
+
   plugins: [react()],
+
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+
+  server: {
+    port: 5173,
+    open: true,
+  },
 })
